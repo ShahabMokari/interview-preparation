4ca957339def275060fcd9f684baff7b2f6da313
shenzhun <shenzhunallen@gmail.com>
2013-10-16 21:13:32 +0800

    selection sort

---
+++ sorting-searching/mocktest/selectionSort.py
@@ -0,0 +1,23 @@
+def selectionSort(A):
+	"""
+	Selection sorting demo using python.
+
+	If non digits in the list, ascii order will be used
+	"""
+        if isinstance(A, list) == False:
+		print 'pls enter a list'
+		return
+	if len(A) <= 1:
+		return A
+
+	for i in range(len(A)-1):
+		#set default min number
+		min = i
+		for j in range(i, len(A)):
+			if A[j] < A[min]:
+				min = j  # find the min item one by one
+
+				#find the min item and exchange min and default one
+		if min != i:
+			A[min], A[i] = A[i], A[min]
+	return A

c300ee5f33374ff769d258fe2f9a4cb2a834eb0e
shenzhun <shenzhunallen@gmail.com>
2013-12-09 22:16:47 +0800

    update binary_search

--- sorting-searching/learning/binary_search.py
+++ sorting-searching/learning/binary_search.py
@@ -1,27 +1,17 @@
-def binarySearch(A, K):
-	"""Sorted list using binary search"""
-        # test if the list is empty
-        if len(A) == 0:
-		print 'list is empty'
+def bs(A, a):
+	if len(A) < 1:
+		print "list is empty"
 		return 
-	# calculate midpoint 
-	mid = len(A)//2
-	if mid ==0 and  K != A[mid]:
-		print 'no match found'
-		return
+	elif A[0] <= a and A[-1] <= a:
+		mid = len(A)//2
+		print mid
 
-	# three way comparsion
-	# key has been found
-	if K == A[mid]:
-		print 'find value', K
-	# key is in lower subset
-	elif K < A[mid]:
-		binarySearch(A[:mid], K)
-	else:
-	# key is in upper subset
-		binarySearch(A[mid:], K)
+		if A[mid] == a:
+			print A[mid]
+		elif A[mid] > a:
+			bs(A[:mid], a)
+		elif A[mid] < a:
+			bs(A[mid:], a)
 
-if __name__ == '__main__':
-	n1= []
-	n = [1, 2, 3, 4, 5, 6, 7, 8]
-	print binarySearch(n, 8)
+
+	
